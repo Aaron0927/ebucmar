0c57c4edb821714c0d61e31b35847fd31a7813d0
Chengfei Zhang <chengfeizh@gmail.com>
2015-04-10 18:46:38 +0800

    edit018

--- ramcube_config/Start_client_test.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-mcbenchmark=/root/mc-benchmark-linux/mc-benchmark
-
-$mcbenchmark -n 10000 -c 50 -h 127.0.0.1 -p 11121 > Result_8.txt &
-$mcbenchmark -n 10000 -c 50 -h 127.0.0.1 -p 11124 > Result_9.txt &
-
